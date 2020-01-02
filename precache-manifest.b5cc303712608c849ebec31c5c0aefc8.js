62c8c69cb53365d4c79fd90a6a14afec97317a59
Nagendram Kisanth <krish1988@live.com>
2020-01-02 20:52:05 +0100

    initial commit

---
+++ precache-manifest.b5cc303712608c849ebec31c5c0aefc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1245e2b1ada2de3acea4dd59755a1566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d741992f270af07a021",
+    "url": "/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "2f37a3b302e12e043902",
+    "url": "/static/js/2.61a37956.chunk.js"
+  },
+  {
+    "revision": "66beb09bd5c53ddc0928ab7844805f0e",
+    "url": "/static/js/2.61a37956.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d741992f270af07a021",
+    "url": "/static/js/main.7e8a60aa.chunk.js"
+  },
+  {
+    "revision": "1a85c09c3f4d829461e5",
+    "url": "/static/js/runtime-main.0b6a28bd.js"
+  },
+  {
+    "revision": "f45c8c538309021ef2436739d45e6906",
+    "url": "/static/media/slide_one.f45c8c53.jpg"
+  },
+  {
+    "revision": "44f54a0d927c304f9e0464c297c43a81",
+    "url": "/static/media/slide_three.44f54a0d.jpg"
+  },
+  {
+    "revision": "c4a3a892899f69b61d2a957c22a9512e",
+    "url": "/static/media/slide_two.c4a3a892.jpg"
+  }
+]);
